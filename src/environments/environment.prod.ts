fd7a0fb335d49e7734a587cac7b8dbf44bf3ba4b
robert.lindoso <robert.lindoso@conecthus.org.br>
2025-07-22 12:41:47 -0400

    feat: adding production url

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://planning-poker-backend-api.fly.dev',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://planning-poker-backend-api.fly.dev',
+};
